9397c64f2ad26020b4097cf00b079c8465d53fbf
luckh-coder <luckhero@gmail.com>
2025-07-03 08:14:44 +0200

    update

--- IDMSproject/IDMSapp/urls.py
+++ IDMSproject/IDMSapp/urls.py
@@ -1,6 +1,7 @@
 
 # urls.py
 from django.urls import path, include
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from .views import *
